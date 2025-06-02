35067217bba0ed1d614bc05ade8e08b7f707ea6d
parisan <parisan0487@gmail.com>
2025-06-02 13:25:15 +0330

    admin

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order");
-const { protect } = require("../middleware/authMiddleware");
+const { protect, adminProtect } = require("../middleware/authMiddleware");
 
 
 router.post("/", protect, async (req, res) => {
@@ -10,7 +10,7 @@ router.post("/", protect, async (req, res) => {
         if (!req.body.items || req.body.items.length === 0) {
             return res.status(400).json({ message: "سبد خرید خالی است" });
         }
-        
+
         const order = await Order.create({ ...req.body, userId });
         res.status(201).json(order);
     } catch (err) {
@@ -21,12 +21,26 @@ router.post("/", protect, async (req, res) => {
 
 router.get("/user-orders", protect, async (req, res) => {
     try {
-      const orders = await Order.find({ userId: req.user._id }).sort({ createdAt: -1 });
-      res.json(orders);
+        const orders = await Order.find({ userId: req.user._id }).sort({ createdAt: -1 });
+        res.json(orders);
     } catch (err) {
-      res.status(500).json({ message: "خطا در دریافت سفارشات", error: err.message });
+        res.status(500).json({ message: "خطا در دریافت سفارشات", error: err.message });
     }
-  });
-  
+});
+
+
+
+router.get("/", protect, adminProtect, async (req, res) => {
+    try {
+        const orders = await Order.find({})
+            .sort({ createdAt: -1 })
+            .populate("userId", "name phone");
+
+        res.json(orders);
+    } catch (err) {
+        res.status(500).json({ message: "خطا در دریافت سفارشات", error: err.message });
+    }
+});
+
 
 module.exports = router;
