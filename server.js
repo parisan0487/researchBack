07eecfdb6d16b2f442913947b6f18f82bfc4ec90
parisan <parisan0487@gmail.com>
2025-06-05 19:50:30 +0330

    server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
+
+// Load Routes
 const userRoutes = require("./src/routes/userRoutes");
 const productRoutes = require("./src/routes/productRoutes");
 const cartRoutes = require("./src/routes/cartRoutes");
@@ -9,25 +11,31 @@ const paymentRoutes = require("./src/routes/paymentRoutes");
 const verifyRoute = require("./src/routes/verifyRoute");
 const ordersRouter = require("./src/routes/orderRoutes");
 const adminRoutes = require("./src/routes/adminRoutes");
-const upload = require("./src/routes/upload");
+const uploadRoutes = require("./src/routes/upload");
 
-
+// Load Env and DB
 dotenv.config();
 connectDB();
+
 const app = express();
+
+// Middleware
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://research-pied.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-const corsOptions = {
-  origin: ["http://localhost:3000", "https://research-pied.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
-
+// Default Route
 app.get("/", (req, res) => {
   res.send("Backend is Running!");
 });
+
+// API Routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
@@ -35,11 +43,10 @@ app.use("/api", paymentRoutes);
 app.use("/api", verifyRoute);
 app.use("/api/orders", ordersRouter);
 app.use("/api/admin", adminRoutes);
-app.use("/api/upload", upload);
+app.use("/api/upload", uploadRoutes);
 
-
-
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
